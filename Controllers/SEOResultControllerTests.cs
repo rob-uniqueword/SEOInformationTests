ded4dcce05f58eec5b7ad8b7fedd375dec852154
Rob <robjohn135@gmail.com>
2020-12-20 23:34:19 +0000

    initial-commit

---
+++ Controllers/SEOResultControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SEOInformation.Utilities;
+using System.Collections.Generic;
+
+namespace SEOInformation.Controllers.Tests
+{
+    [TestClass()]
+    public class SEOResultControllerTests
+    {
+        [TestMethod()]
+        public void SEOResultControllerTest()
+        {
+            var searchString = "testSearchString";
+            var targetURL = "testTargetURL";
+
+            var seoResult = new SEOResult("Google", searchString, targetURL, new List<int> { 1 });
+            var mockSearchScraper = new Mock<ISearchScraper>();
+            var mockSearchScraperFactory = new Mock<ISearchScraperFactory>();
+            var mockLogger = new Mock<ILogger<SEOResultController>>();
+
+            mockSearchScraper.Setup(x => x.GetSEOResults(It.IsAny<string>(), It.IsAny<string>())).Returns(seoResult);
+            mockSearchScraperFactory.Setup(x => x.GetSearchScraper(It.IsAny<SearchProvider>())).Returns(mockSearchScraper.Object);
+
+            var seoResultController = new SEOResultController(mockLogger.Object, mockSearchScraperFactory.Object);
+
+            var result = seoResultController.Get("Google", searchString, targetURL);
+
+            mockSearchScraperFactory.Verify(x => x.GetSearchScraper(SearchProvider.Google), Times.Once);
+            mockSearchScraper.Verify(x => x.GetSEOResults(searchString, targetURL));
+            Assert.AreEqual(result, seoResult);
+        }
+    }
+}
---
+++ Utilities/HTMLRequestorTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SEOInformation.Utilities.Tests
+{
+    [TestClass()]
+    public class HTMLRequestorTests
+    {
+        [TestMethod()]
+        public void GetHtmlAsyncTest()
+        {
+            var htmlRequestor = new HTMLRequestor();
+            var html = htmlRequestor.GetHtmlAsync("https://www.google.co.uk/search?num=100&q=land+registry+search").Result;
+            Assert.IsFalse(String.IsNullOrWhiteSpace(html));
+        }
+    }
+}
